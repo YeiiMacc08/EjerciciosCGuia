16503fad7d7163ad0749acb871de59d3efcf112e
YeiiMacc08 <50213870+YeiiMacc08@users.noreply.github.com>
2019-05-20 23:23:45 -0500

    Add files via upload

---
+++ Ejercicio 5 - 124/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_5___124
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var car = "";// se cambio el tipo de dato de char a var
+
+            Console.Write("caracter: ");
+            car = Console.ReadLine();
+            Console.WriteLine($"{car}"); // se llamo la variable
+
+            // se eliminaron las 3 lineas de codigo que estaban repetidas
+
+            Console.ReadLine();// se agrego un readline
+
+        }
+    }
+}
