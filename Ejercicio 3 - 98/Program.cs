21250bfead81fda42e985178efca2bd8b99e9d92
YeiiMacc08 <50213870+YeiiMacc08@users.noreply.github.com>
2019-05-20 23:20:01 -0500

    Add files via upload

---
+++ Ejercicio 3 - 98/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_3___98
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float gradosC = 0;
+
+            Console.WriteLine("Digite los grados Centigrados");
+
+            gradosC = int.Parse(Console.ReadLine());
+            CGrados grados = new CGrados();
+            Console.WriteLine(CGrados.obtenerfahrenheit(gradosC));
+
+            Console.ReadLine();
+
+            gradosC = 0;
+
+            Console.WriteLine(CGrados.obtenerfahrenheit(gradosC));
+
+            Console.ReadLine();
+        }
+    }
+}
